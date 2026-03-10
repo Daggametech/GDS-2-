fb910f563af443594d704ffb749e3d5b72140f33
Daggametech <dbecho@email.neit.edu>
2026-03-10 12:08:56 -0400

    Update player.js

--- js/Data/player.js
+++ js/Data/player.js
@@ -86,7 +86,7 @@ var playerData ={
 		//The attack animation 
 		attack:
 		{
-			fps:30,
+			fps:60,
 			cycle:false,
 			//width:300,
 			frames:
@@ -112,6 +112,7 @@ var playerData ={
 		
 
 }
+
 
 
 
